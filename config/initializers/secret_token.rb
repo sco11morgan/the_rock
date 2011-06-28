f49ad5574694afb257c4cddf9ae5e9db18734c22
Scott Morgan <morgandev@gmail.com>
2011-06-27 20:28:26 -0700

    baseline commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheRock::Application.config.secret_token = '8317a62e1be6e300bfb3dab7cf7ba16d9aa5cfa2df3699165540ef3f47c72f0edc0219e718a916ddf6ceeca8788ce422a9ae52c364c4790cf601b8180bd20e55'
